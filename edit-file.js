226e201fea8fa3c58e2757bb3f48734b697c6864
Benjamin Clark <benisin@gmail.com>
2019-08-14 16:57:13 -0700

    FINISHED

--- edit-file.js
+++ edit-file.js
@@ -5,64 +5,45 @@ const fsExtra = require('fs-extra');
 
 module.exports = exports = {};
 
-exports.readFile = (fileName, callback) => {
-  fs.readFile(fileName, (error, fileContents) => {
-    if (error) {
-      callback(error);
-    } else {
-      callback(undefined, JSON.parse(fileContents));
-    }
-  });
-}
-
-
 const fakeObject = JSON.stringify({
   firstName: 'fake',
   lastName: 'fake',
   hair: { type: 'fake', color: 'fake' },
   favoriteFoods: ['fake', 'fake', 'fake'],
   married: false,
-  kids: 0
-})
-
-const originalObject = JSON.stringify({
-  firstName: 'Edward',
-  lastName: 'Scissorhands',
-  hair: { type: 'wavy', color: 'brown' },
-  favoriteFoods: ['pizza', 'cupcakes', 'children'],
-  married: false,
-  kids: 0
-})
+  kids: 0,
+});
 
 
-exports.editFile = (fileName, callback) => {
-
-  fs.writeFile(fileName, fakeObject, function (err) {
-    if (err) {
-      return console.log(err);
+exports.readFile = (fileName, callback) => {
+  fs.readFile(fileName, (error, fileContents) => {
+    if (error) {
+      callback(error);
     } else {
-      callback("The file was saved!");
+      callback(undefined, fileContents);
+    }
+  });
+}
+
+exports.writeFile = (filePath, editedObject, callback) => {
+
+  fs.writeFile(filePath, editedObject, (error, ) => {
+    if (error) {
+      callback(error);
+    } else {
+      callback(undefined, JSON.parse(editedObject));
     }
   });
 
 }
 
 
-class FileSystem {
 
-  readFile(fileName, callback) {
-    fs.readFile(fileName, (error, fileContents) => {
-      if (error) {
-        callback(error);
-      } else {
-        // console.log(fileContents.toString());
-        callback(undefined, fileContents.toString());
-      }
-    });
-  }
-
-  readFilePromises(fileName) {
-    // Vinicio - this line sets up two callbacks: rejection  and success
-    return fsExtra.readFile(fileName);
-  }
+exports.readFilePromises = (fileName) => {
+  return fsExtra.readFile(fileName);
 }
+
+exports.writeFilePromises = (filePath, editedObject) => {
+  return fsExtra.writeJson(filePath, editedObject)
+}
+
--- index.js
+++ index.js
@@ -1,30 +1,63 @@
 'use strict';
 
-const fileSystem = require('./edit-file')
-
-fileSystem.readFile(`${__dirname}/data/person.json`, (error, data) => {
-  if (error) {
-    console.error('ERROR');
-  } else {
-    console.log(data);
-  }
-});
+const myFileSystem = require('./edit-file');
 
 
-fileSystem.editFile(`${__dirname}/data/person.json`, (error, data) => {
-  if (error) {
-    console.error('ERROR');
-  } else {
-    console.log(data);
-  }
-});
-
-fileSystem.readFile(`${__dirname}/data/person.json`, (error, data) => {
-  if (error) {
-    console.error('ERROR');
-  } else {
-    console.log(data);
-  }
-});
+// ---------- WITH CALLBACKS ----------
 
 
+
+// myFileSystem.readFile(`${__dirname}/data/person.json`, (error, fileString) => {
+//   if (error) {
+//     console.error('readFile ERROR');
+//   } else {
+//     let originalObject = JSON.parse(fileString);
+//     console.log('Original file', originalObject);
+
+//     // edit file 
+//     let editedFile = editFile(originalObject);
+
+//     // write file
+//     myFileSystem.writeFile(`${__dirname}/data/person.json`, JSON.stringify(editedFile), (error, data) => {
+//       if (error) {
+//         console.error('myFileSystem.editFile ERROR', error);
+//       } else {
+//         console.log('edited file with callbacks', data);
+//       }
+//     });
+
+//   }
+// });
+
+
+// ---------- WITH PROMISES ----------
+
+function editFile(file) {
+  file.firstName = 'fake';
+  file.lastName = 'fake';
+  return file;
+}
+
+
+myFileSystem.readFilePromises(`${__dirname}/data/person.json`)
+  .then(fileString => {
+    console.log('Read file with Promises', JSON.parse(fileString));
+    return JSON.parse(fileString);
+  })
+  // edit file
+  .then(file => {
+    return editFile(file);
+  })
+  // write file
+  .then(editedFile => {
+    myFileSystem.writeFilePromises(`${__dirname}/data/person.json`, editedFile);
+  })
+  // read file again
+  .then(result => {
+    console.error('result', result);
+    return myFileSystem.readFilePromises(`${__dirname}/data/person.json`);
+  })
+  .then(fileString => {
+    console.log('Write file with Promises', JSON.parse(fileString));
+  })
+  .catch(error => console.error('ERROR-d3kg', error));
